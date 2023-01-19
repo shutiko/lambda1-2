85d2744a51bca4666dcca5d416a04297a5ae3c44
Borblik Oleg <likorff@list.ru>
2023-01-19 19:01:54 +0700

    first commit

---
+++ Worker.java
@@ -0,0 +1,34 @@
+package lambdaTask1_2;
+
+public class Worker {
+
+    private OnTaskErrorListener errorCallback;
+
+    private OnTaskDoneListener callback;
+
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is error");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String error);
+    }
+}
